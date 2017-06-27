240053499028315a2bfa085cff2e37df1a05f572
Henry <henryblanchette@markov.reed.edu>
2017-06-26 22:23:07 -0700

    more cool multitrait genealogy pics

--- create_genealogy.py
+++ create_genealogy.py
@@ -4,18 +4,18 @@ import genealogy, genealogy_multitraits, csv_creator, dot_creator
 ### MULTI TRAIT ###
 ###################
 
-generations = 40
+generations = 50
 generation_sizes = 10
 
-a = 1.5
+a = 1.8
 p = 1.1
-t = 0.6
+t = 0.4
 parents = 2
 traits = [1.2,1.4,1.6]
 
 def multitrait(dot,csv):
     genealogy_multitraits.init_genealogy()
-    gen_data = genealogy_multitraits.make_genealogy( name="MULTITRAIT_prod_5", generations=generations, generation_sizes_function=lambda x: generation_sizes ,parents=parents, trait_factor=t, popular_factor=p, age_factor=a, traits=traits)
+    gen_data = genealogy_multitraits.make_genealogy( name="MULTITRAIT_prod_6", generations=generations, generation_sizes_function=lambda x: generation_sizes ,parents=parents, trait_factor=t, popular_factor=p, age_factor=a, traits=traits)
     if dot:
         dot_creator.create_dot(gen_data,True)
     if csv:
